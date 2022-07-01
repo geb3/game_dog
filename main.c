c7a809bd7d9ca8907cbf4fd7b78d32738d0db82b
George <gosha738g@gmail.com>
2022-07-01 17:45:41 +0300

    v='1.1'

--- main.c
+++ main.c
@@ -22,7 +22,7 @@ int getch() {
 #endif
 int main() {
     srand(time(NULL));
-    char map[10][21];
+    char map[20][61];
     int i;
     int x = 10, y = 5;
     int ox, oy;
@@ -30,31 +30,32 @@ int main() {
     char key;
     int score = 0;
     do {
-        sprintf(map[0], "####################");
-        for (i = 1; i < 9; i++) {
-            sprintf(map[i],"#                  #");
+        sprintf(map[0], "|==========================================================|");
+        for (i = 1; i < 19; i++) {
+            sprintf(map[i],"|                                                          |");
         }
-        sprintf(map[9], "####################");
+        sprintf(map[19], "|==========================================================|");
         
         map[y][x] = '@';
         map[ay][ax] = '*';
         system("clear");
-        printf("      Score %d\n", score);
-        for (i = 0; i < 10; i++) printf("%s\n", map[i]);
+        printf("\t\t\t  Score %d\n", score);
+        for (i = 0; i < 20; i++) printf("%s\n", map[i]);
         ox = x;
         oy = y;
+        printf("\tW: up, A: left, S: down, D: right, E: exit\n");
         key = getch();
         if (key == 'w') y--;
         if (key == 's') y++;
         if (key == 'a') x--;
         if (key == 'd') x++;
-        if (map[y][x] == '#') {
+        if ((map[y][x] == '|') || (map[y][x] == '=')) {
             x = ox;
             y = oy;
         }
         if ((x == ax) && (y == ay)) {
-            ax = rand() % 18 + 1;
-            ay = rand() % 8 + 1;
+            ax = rand() % 58 + 1;
+            ay = rand() % 18 + 1;
             score++;
         }
 
